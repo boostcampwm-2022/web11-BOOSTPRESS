e47da4a6e499f645bbcd8d09013a177af36c06b7
Themion <themion@naver.com>
2022-11-10 14:32:00 +0900

    feat(root): eSLint와 Prettier 설정
    NestJS에 있던 ESLint와 Prettier 설정을 프로젝트 루트 폴더로 이동했습니다. ESLint 설정은 기존에 있던 설정에, 추가적으로 quote 설정과 indent
    설정을 추가하였습니다.
    
    #1

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint'],
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    root: true,
+    env: {
+        node: true,
+        jest: true,
+    },
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {
+        indent: ['error', 4, { SwitchCase: 1 }],
+        quotes: ['error', 'single'],
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+    },
+};

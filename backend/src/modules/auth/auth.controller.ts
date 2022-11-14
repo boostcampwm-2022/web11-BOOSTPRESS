409f66d5181a50047f431619bf62ce9d262e9f44
Themion <themion@naver.com>
2022-11-15 01:13:38 +0900

    feat(backend): guard 적용
    nestjs에서 제공하는 guard 기능을 이용해 인증 과정을 nestjs에 위임

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { User } from 'src/decorator';
+import { GitHubGuard } from 'src/guard/github.guard';
 
 @Controller('auth')
 export class AuthController {
+    @UseGuards(GitHubGuard)
     @Get('github')
     async github(@User() user) {
         return user;

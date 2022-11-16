279874293e23cdc210e06bfc19cc924e7ddd9af6
supersfel <jmg081005@naver.com>
2022-11-16 15:35:37 +0900

    style(fe): 의미에 맞게 styled 이름 변경

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,20 @@
 /* Header */
 import React from 'react';
 import styled from '@emotion/styled';
-import { BasicButton } from '../styles/common';
+import { BasicInput } from '../styles/common';
+
+// 로고는 안보일때도 있을 수 있음
+const Header = ({ isLogoActive }: headerType) => {
+    return (
+        <HeaderWrapper>
+            <LogoArea isLogoActive={isLogoActive}>
+                <LogoTitle>BOOSTPRESS</LogoTitle>
+                The Ultimate Platform For Dev
+            </LogoArea>
+            <SearchArea placeholder="검색어 입력 " />
+        </HeaderWrapper>
+    );
+};
 
 const HeaderWrapper = styled.div`
     display: flex;
@@ -25,23 +38,9 @@ const LogoTitle = styled.h1`
     margin-bottom: 0.5rem;
 `;
 
-const SearchArea = styled(BasicButton)`
+const SearchArea = styled(BasicInput)`
     width: 358px;
     font-size: 15px;
     padding: 1rem;
 `;
-
-// 로고는 안보일때도 있을 수 있음
-const Header = ({ isLogoActive }: headerType) => {
-    return (
-        <HeaderWrapper>
-            <LogoArea isLogoActive={isLogoActive}>
-                <LogoTitle>BOOSTPRESS</LogoTitle>
-                The Ultimate Platform For Dev
-            </LogoArea>
-            <SearchArea placeholder="검색어 입력 " />
-        </HeaderWrapper>
-    );
-};
-
 export default Header;
--- frontend/src/styles/common.ts
+++ frontend/src/styles/common.ts
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
-export const BasicButton = styled.button`
+export const BasicInput = styled.input`
     background: #ffffff;
     border: 2px solid #e8e8e8;
     border-radius: 10px;

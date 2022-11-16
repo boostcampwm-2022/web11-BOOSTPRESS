87805925e5d190658925045e238c872e62b779b4
kasterra <kasterra@naver.com>
2022-11-16 12:20:20 +0900

    chore : 필요 없는 파일 삭제

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import { Route, Routes } from 'react-router-dom';
-import Main from './containers/Main';
+import Main from './pages/Main';
 
 const App = () => {
     return (

f63e5d8957b80b793640c45c6d265114854413f4
Themion <themion@naver.com>
2022-11-22 22:20:14 +0900

    feat(backend): 게시글 작성 API의 명세를 swagger에 표시
    게시글 작성 시 body의 요구사항과 실행 결과를 swagger를 이용해 표시
    
    feat #15

--- backend/src/modules/article/article.controller.ts
+++ backend/src/modules/article/article.controller.ts
@@ -1,13 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { User } from '@prisma/client';
 import { CurrentUser } from 'src/decorator';
 import { ArticleService } from './article.service';
 import { PostArticle } from './dto';
+import { Write } from './swagger';
 
 @Controller('article')
 export class ArticleController {
     constructor(private readonly articleService: ArticleService) {}
 
+    @ApiOperation(Write.Operation)
+    @ApiResponse(Write._200)
+    @ApiResponse(Write._401)
     @Post('')
     async write(@CurrentUser() user: User, @Body() dto: PostArticle) {
         return await this.articleService.commit(user, dto);
---
+++ backend/src/modules/article/swagger/write.swagger.ts
@@ -0,0 +1,20 @@
+import { UnauthorizedException } from '@nestjs/common';
+import { ApiOperationOptions, ApiResponseOptions } from '@nestjs/swagger';
+
+export const Operation: ApiOperationOptions = {
+    summary: '게시글 작성 API',
+    description:
+        '게시글을 작성해 .md 파일로 저장한 뒤, GitHub API를 이용해 작성한 게시글을 commit한다.',
+};
+
+export const _200: ApiResponseOptions = {
+    status: 200,
+    description: 'GitHub API로 commit한 결과',
+    type: typeof {},
+};
+
+export const _401: ApiResponseOptions = {
+    status: 401,
+    description: '로그인되어 있지 않음',
+    type: UnauthorizedException,
+};
